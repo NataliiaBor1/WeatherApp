0fa58b258b258fec3b470edcc34d8838a21f2d72
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-05-03 12:01:57 -0700

    weatherapp.js was changed

---
+++ weatherapp.js
@@ -0,0 +1,21 @@
+const apiURL = "https://open-meteo.com/v1/forecast?";
+const latitude = 37.6819;
+const longitude = -121.768;
+const temperature = "temperature_2m";
+
+fetch(`${apiURL}latitude=${latitude}&longitude=${longitude}&hourly=${temperature}`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': application/json
+    }
+}) 
+    .then(response => {
+        if (response.ok) {
+            console.log('Here is your forecast for the week')
+        } else {
+            console.log('Something went wrong... Try again')
+        }
+        response.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('error'))
