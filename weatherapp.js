ebc0430c94ee7c46ad250fb37821e7090871d5e5
Nataliia B <114429981+NataliiaBor1@users.noreply.github.com>
2024-05-08 12:04:33 -0700

    weatherapp.js was changed, index.html was changed (#10)
    * index.html was created
    
    * index.html was changed
    
    * index.html was changed, weather.css was created, weatherapp.js was created
    
    * images were added in special folder
    
    * images names were changed
    
    * Update README.md
    
    * index.html was changed, icon was added
    
    * images folder was refactored
    
    * weatherapp.js was changed
    
    * weatherapp.js was changed, index.html was changed
    
    * weatherapp.js was changed, index.html was changed

--- weatherapp.js
+++ weatherapp.js
@@ -8,10 +8,11 @@ fetch(`${apiURL}latitude=${latitude}&longitude=${longitude}&hourly=${temperature
     headers: {
         'Content-Type': application/json
     }
-}) 
+} 
     .then(response => {
         if (response.ok) {
             console.log('Here is your forecast for the week')
+            console.log(response)
         } else {
             console.log('Something went wrong... Try again')
         }
@@ -19,3 +20,4 @@ fetch(`${apiURL}latitude=${latitude}&longitude=${longitude}&hourly=${temperature
     })
     .then(data => console.log(data))
     .catch(error => console.log('error'))
+);
