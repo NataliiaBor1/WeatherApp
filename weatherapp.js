d3a711bdc289a0812b56e036150cb18448f40d55
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-05-21 18:45:39 -0700

    weatherapp.js was changed, index.html was changed

--- weatherapp.js
+++ weatherapp.js
@@ -3,8 +3,78 @@ const latitude = 37.6819;
 const longitude = -121.768;
 const temperature = "temperature_2m";
 const precipitation = "precipitation,rain,showers,snowfall";
+const temperatureUnit = "fahrenheit";
+const windSpeed = "ms";
+const timezone = "America%2FLos_Angeles";
+const forecastDays = "1";
+const forecastForm = document.querySelector(".forecastForm");
+const locationInput = document.querySelector(".locationInput");
+const card = document.querySelector(".card");
 
-fetch(`${apiURL}latitude=${latitude}&longitude=${longitude}&hourly=${temperature}`, {
+forecastForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const location = locationInput.value;
+
+    if(location){
+        try{
+            const forecastData = await getForecastData(location);
+            showForecastData(forecastData);
+
+        }
+        catch(error){
+            console.error(error);
+            showError(error);
+        }
+
+    }
+    else{
+        showError("Please, enter your location");
+    }
+
+});
+
+async function getForecastData(location){
+
+    const url = `${apiURL}latitude=${latitude}&longitude=${longitude}&current=${temperature}&hourly=${temperature}&temperature_unit=${temperatureUnit}&wind_speed_unit=${windSpeed}&timezone=${timezone}&forecast_days=${forecastDays}`;
+
+    const res = await fetch(url);
+
+    console.log(res);
+
+    if (!res.ok) {
+        throw new Error("That is not possible to get data")
+    }
+
+    return await response.json();
+}
+
+function showForecastData(data){
+
+    console.log(data);
+
+    
+
+
+
+}
+
+function getForecastPicture(forecastIndex){
+
+}
+
+function showError(errorMessage){
+
+    const error = document.createElement("p");
+    error.textContext = errorMessage;
+    error.classList.add("errorInfo");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(error);
+}
+
+fetch(`${apiURL}latitude=${latitude}&longitude=${longitude}&current=${temperature}&hourly=${temperature}&temperature_unit=${temperatureUnit}&wind_speed_unit=${windSpeed}&timezone=${timezone}&forecast_days=${forecastDays}`, {
     method: 'GET',
     headers: {
         'Content-Type': application/json
@@ -17,7 +87,7 @@ fetch(`${apiURL}latitude=${latitude}&longitude=${longitude}&hourly=${temperature
         } else {
             console.log('Something went wrong... Try again')
         }
-        response.json()
+        response.json();
     })
     .then(data => {
         data.forEach(temp => {
